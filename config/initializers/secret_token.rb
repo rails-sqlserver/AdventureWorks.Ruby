e208a19d1520adc5de7bed3dd0f8770f9e26df78
Ken Collins <ken@metaskills.net>
2011-02-14 15:34:00 -0500

    New rails 3.0.4 project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdventureWorks::Application.config.secret_token = 'eae35b092d57c90906bdf90a8edd1a1e1a192edcda0d7efb1ae1b5345692b1e82c706b012fe249196e8198e76d2757e81f0affc602574a7d0f8707f0fdaa5c7f'
